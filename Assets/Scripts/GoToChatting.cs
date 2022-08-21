e3e9b83d33f7fd8db2e9858bf761d0f994656330
ruddus7207 <kimgy05221@daum.net>
2022-08-21 14:58:59 +0900

    채팅구현

---
+++ Assets/Scripts/GoToChatting.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToChatting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("로켓 탑승 애니메이션 보여주기 -> 씬 전환 ");
+        SceneManager.LoadScene("ChattingScene");
+    }
+}
